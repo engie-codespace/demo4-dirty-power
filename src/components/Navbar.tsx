c863bb00b9e05e04d8f48520c7b50e8a68d50451
demo4-poweracme <demo4.poweracme@gmail.com>
2025-06-11 08:19:33 +0000

    Set website and navbar background color to pink

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar: React.FC = () => {
   };
 
   return (
-    <nav className="bg-primary shadow-lg">
+    <nav className="bg-pink-500 shadow-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className="flex items-center">
